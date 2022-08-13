f66292927fddacb646c00ad81a23a6b47460917e
Felipe Coutinho <felipi@live.com>
2022-08-13 15:25:15 -0300

    update selectfeed

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ export default function App() {
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-
     setValues({
       ...values,
       [name]: value,
@@ -26,6 +25,48 @@ export default function App() {
 
   const [select, setSelect] = React.useState('');
   const [select1, setSelect1] = React.useState('');
+  const options = [
+    {
+      label: "eth1",
+      value: "eth1",
+    },
+    {
+      label: "eth2",
+      value: "eth2",
+    },
+    {
+      label: "eth3",
+      value: "eth3",
+    },
+    {
+      label: "eth4",
+      value: "eth4",
+    },
+    {
+      label: "eth5",
+      value: "eth5",
+    },
+    {
+      label: "eth6",
+      value: "eth6",
+    },
+    {
+      label: "eth7",
+      value: "eth7",
+    },
+    {
+      label: "eth8",
+      value: "eth8",
+    },
+    {
+      label: "xeth1",
+      value: "xeth1",
+    },
+    {
+      label: "xeth2",
+      value: "xeth2",
+    },
+  ];
 
   return (
     <div>
@@ -38,33 +79,23 @@ export default function App() {
       <p>VLAN PON 7 <input type="number" value={values.vlanpon7} onChange={handleInputChange} name="vlanpon7" /></p>
       <p>VLAN PON 8 <input type="number" value={values.vlanpon8} onChange={handleInputChange} name="vlanpon8" /></p>
 
+      <select className="selectcomando" value="teste" onChange={({ target }) => setSelect(target.value)}>
+        {options.map((option) => (
+          <option value={option.value}>{option.label}</option>
+        ))}
+      </select><br></br>
+
+      <select className="selectcomando" value={select1} onChange={({ target }) => setSelect1(target.value)}>
+        {options.map((option) => (
+          <option value={option.value}>{option.label}</option>
+        ))}
+      </select>
+
       <div className="bridge-uplink">
         <h1>Bridges Uplink</h1>
         <h2>
-          <p>bridge add <select className="selectcomando" value={select} onChange={({ target }) => setSelect(target.value)} id="produtos">
-            <option value="eth1">eth1</option>
-            <option value="eth2">eth2</option>
-            <option value="eth3">eth3</option>
-            <option value="eth4">eth4</option>
-            <option value="eth5">eth5</option>
-            <option value="eth6">eth6</option>
-            <option value="eth7">eth7</option>
-            <option value="eth8">eth8</option>
-            <option value="xeth1">xeth1</option>
-            <option value="xeth2">xeth2</option>
-          </select> uplink vlan {values.vlanpon1} tagged</p>
-          <p>bridge add <select className="selectcomando" value={select1} onChange={({ target }) => setSelect1(target.value)} id="produtos">
-            <option value="eth1">eth1</option>
-            <option value="eth2">eth2</option>
-            <option value="eth3">eth3</option>
-            <option value="eth4">eth4</option>
-            <option value="eth5">eth5</option>
-            <option value="eth6">eth6</option>
-            <option value="eth7">eth7</option>
-            <option value="eth8">eth8</option>
-            <option value="xeth1">xeth1</option>
-            <option value="xeth2">xeth2</option>
-          </select> uplink vlan {values.vlanpon2} tagged</p>
+          <p>bridge add {select} uplink vlan {values.vlanpon1} tagged</p>
+          <p>bridge add {select1} uplink vlan {values.vlanpon2} tagged</p>
           <p>bridge add eth1 uplink vlan {values.vlanpon3} tagged</p>
           <p>bridge add eth1 uplink vlan {values.vlanpon4} tagged</p>
           <p>bridge add eth1 uplink vlan {values.vlanpon5} tagged</p>
